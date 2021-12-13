f7f8b6574859d18ca4ccc5c2f6a550e7375fc528
aureliaregita <95738602+aureliaregita@users.noreply.github.com>
2021-12-13 16:04:42 +0700

    Update Coba_UAS.py

--- Coba_UAS.py
+++ Coba_UAS.py
@@ -140,7 +140,8 @@ plt.plot(df1['tahun'], df1['produksi'])
 plt.xlabel("Tahun")
 plt.ylabel("Jumlah Produksi")
 
-col9,col10=st.columns([3,1]
+
+col9,col10=st.columns([3,1])
 col9.pyplot(fig)
 
 with st.expander("Deskripsi"):
